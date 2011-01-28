a8cf7930337cbeb8ee1bb2783940741bb18ab98c
postpostmodern <postpostmodern@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-28 17:49:48 +0000

    git-svn-id: https://plugins.svn.wordpress.org/aitch-ref/trunk@338243 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- _aitch-ref.php
+++ _aitch-ref.php
@@ -3,7 +3,7 @@
 Plugin Name: aitch-ref!
 Plugin URI: http://wordpress.org/extend/plugins/aitch-ref/
 Description: href junk. Requires PHP 5.
-Version: 0.45
+Version: 0.46
 Author: Eric Eaglstun
 Author URI: http://ericeaglstun.com
 */
@@ -13,7 +13,6 @@ add_filter( 'admin_url', 'AitchRef::_site_url' );
 add_filter( 'bloginfo', 'AitchRef::_site_url' );
 add_filter( 'bloginfo_url', 'AitchRef::_site_url' );
 add_filter( 'get_pagenum_link', 'AitchRef::_site_url' );
-add_filter( 'home_url', 'AitchRef::_site_url' );
 add_filter( 'option_url', 'AitchRef::_site_url' );
 add_filter( 'plugins_url', 'AitchRef::_site_url' );
 add_filter( 'post_link', 'AitchRef::_site_url' );
@@ -23,10 +22,11 @@ add_filter( 'url', 'AitchRef::_site_url' );
 add_filter( 'wp_list_pages', 'AitchRef::_site_url' );
 
 // these need to return back with leading http://
-add_filter( 'get_permalink', 'AitchRef::_site_url_absolute' ); // maybe? test
+add_filter( 'get_permalink', 'AitchRef::_site_url_absolute' ); 
+add_filter( 'home_url', 'AitchRef::_site_url_absolute' );
 add_filter( 'option_siteurl', 'AitchRef::_site_url_absolute' );
-add_filter( 'page_link', 'AitchRef::_site_url_absolute' ); // maybe? test
-add_filter( 'pre_post_link', 'AitchRef::_site_url_absolute' ); // maybe? test
+add_filter( 'page_link', 'AitchRef::_site_url_absolute' ); 
+add_filter( 'pre_post_link', 'AitchRef::_site_url_absolute' );
 add_filter( 'site_url', 'AitchRef::_site_url_absolute' );
 add_filter( 'template_directory_uri', 'AitchRef::_site_url_absolute' );
 
