67dca74ebb34524b6f8c00bb26f430bb4d642ade
postpostmodern <postpostmodern@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-24 19:38:52 +0000

    minor changes to test wp svn sync
    git-svn-id: https://plugins.svn.wordpress.org/aitch-ref/trunk@844743 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/admin/options.php
+++ views/admin/options.php
@@ -1,99 +1,23 @@
 <?php
-
-namespace aitchref;
-
 /*
-*	show link to admin options in 'settings' sidebar
-*
+*	Admin settings screen
+*	Version: 0.85
 */
-function admin_menu(){
-	add_options_page( 'aitch ref! Settings', 'aitch ref!', 'manage_options', 'aitch-ref', __NAMESPACE__.'\options_general' );
-}
-add_action( 'admin_menu', __NAMESPACE__.'\admin_menu' );
-
-/*
-*	add 'settings' link in main plugins page
-*	attached to plugin_action_links_* action
-*	@param array
-*	@return array
-*/
-function admin_plugins( $links ){
-	$settings_link = '<a href="options-general.php?page=aitch-ref">Settings</a>';  
-	array_unshift( $links, $settings_link );
-	return $links;
-}
-add_filter( 'plugin_action_links_aitch-ref/_plugin.php', __NAMESPACE__.'\admin_plugins' );
+?>
+<div class="wrap">
+	<h2>aitch ref!</h2>
 	
-/*
-*
-*	@param string
-*	@return array
-*/
-function message( $string = NULL ){
-	static $messages = NULL ;
+	<?php echo $messages; ?>
 	
-	if( is_null($messages) )
-		$messages = array();
+	<p>possible urls seperated by space or new line (include http/s, no trailing slash)</p>
+
+	<form method="post">
+		<textarea name="urls" cols="60" style="background-image:url(<?php echo $path; ?>/ref.jpg);background-repeat:no-repeat;background-position:bottom right; height:377px"><?php echo $urls; ?></textarea>
 		
-	if( !is_null($string) )
-		$messages[] = $string;
+		<a href="http://www.flickr.com/photos/avinashkunnath/2402114514/in/photostream/" style="font-size:.6em">photo by Avinash Kunnath</a>
 		
-	return $messages;
-}
-
-/*
-*	callback for add_options_page() to render options page in admin 
-*
-*/
-function options_general(){
-	if( isset($_POST['urls']) ){
-		update_urls( $_POST['urls'] );
-	}
-	
-	$vars = (object) array();
-	
-	$vars->messages = implode( "\n", message() );
-	$vars->path = plugins_url( '', __FILE__ );
-	$vars->urls = esc_textarea( get_urls() );
-	
-	render( 'admin/options-general', $vars );
-}
-
-/*
-*	render a page into wherever
-*	(only used in admin screen)
-*	@param string
-*	@param object|array
-*	@return
-*/
-function render( $filename, $vars = array() ){
-	$template = __DIR__.'/views/'.$filename.'.php';
-	if( file_exists($template) ){
-		extract( (array) $vars, EXTR_SKIP );
-		include $template;
-	}
-}
-	
-/*
-*
-*	@param string
-*	@return
-*/
-function update_urls( $str ){
-	$urls = preg_split ("/\s+/", $str);
-	$urls = array_map( 'trim', $urls );
-	$urls = array_unique( $urls );
-	sort( $urls );
-	
-	foreach( $urls as $k=>$url ){
-		// no trailing slash!
-		if( strrpos($url, '/') == (strlen($url)-1) ){
-			$urls[$k] = substr( $url, 0, -1 );
-		}
-	}
-	
-	$urls = json_encode( $urls );
-	update_option( 'aitchref_urls', $urls );
-	
-	message( '<div class="updated fade"><p>aitch-ref! updated</p></div>' );
-}
+		<div>
+			<input type="submit" value="Update"/>
+		</div>
+	</form>
+</div>
